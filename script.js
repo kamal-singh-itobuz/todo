f15149d91b312dc22ebaa3892072aed611aa950b
Itobuz <itobuz@Itobuzs-MacBook-Pro.local>
2024-02-15 13:33:48 +0530

    update almost complete

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputField = document.querySelector(".input");
 const addBtn = document.querySelector(".add");
+const updateBtn = document.querySelector('.update');
 const taskSection = document.querySelector(".tasks-section");
 const allOptionsBtns = document.querySelectorAll(".option-btn");
 
@@ -8,12 +9,12 @@ const activeShow = document.querySelector(".active-show");
 const allShow = document.querySelector(".all-show");
 const clearCompleted = document.querySelector(".clear-completed");
 
-const allTasksCount = document.querySelector('.all-count');
-const activeTasksCount = document.querySelector('.active-count'); 
-const completedTasksCount = document.querySelector('.completed-count'); 
+const allTasksCount = document.querySelector(".all-count");
+const activeTasksCount = document.querySelector(".active-count");
+const completedTasksCount = document.querySelector(".completed-count");
 let allOrActiveOrComplete = "All";
 function backgroundChange(buttonName) {
-    allOptionsBtns.forEach((ele) => ele.style.backgroundColor = "#023047");
+    allOptionsBtns.forEach((ele) => (ele.style.backgroundColor = "#023047"));
     buttonName.style.backgroundColor = "#fca311";
 }
 
@@ -26,19 +27,32 @@ function htmlCreation(section, timedate, task, iscomplete) {
     currentTime.setAttribute("class", "current-time");
     currentTime.innerText = timedate;
     const taskDescription = document.createElement("p");
-    taskDescription.setAttribute("class", "task-description text-nowrap overflow-scroll");
+    taskDescription.setAttribute(
+        "class",
+        "task-description text-nowrap overflow-scroll"
+    );
     taskDescription.setAttribute("data-taskid", timedate);
     taskDescription.innerText = task;
     taskCard.append(currentTime, taskDescription);
 
     const editDeleteCompleteBtn = document.createElement("div");
-    editDeleteCompleteBtn.setAttribute("class","d-flex align-items-center gap-4");
+    editDeleteCompleteBtn.setAttribute(
+        "class",
+        "d-flex align-items-center gap-4"
+    );
     const deleteBtn = document.createElement("img");
     deleteBtn.setAttribute("src", "./images/delete.svg");
     deleteBtn.setAttribute("alt", "delete...");
     deleteBtn.setAttribute("class", "delete-btn mx-1 text-white");
     deleteBtn.setAttribute("data-deletetimedate", timedate);
-    editDeleteCompleteBtn.append(deleteBtn);
+    if (!iscomplete) {
+        const editBtn = document.createElement("img");
+        editBtn.setAttribute("src", "./images/edit.svg");
+        editBtn.setAttribute("alt", "edit...");
+        editBtn.setAttribute("class", "edit-btn mx-1 text-white");
+        editBtn.setAttribute("data-edittimedate", timedate);
+        editDeleteCompleteBtn.append(editBtn);
+    }
     const completeBtn = document.createElement("button");
     completeBtn.setAttribute("class", "complete-btn mx-1 text-white");
     completeBtn.setAttribute("data-completetimedate", timedate);
@@ -48,7 +62,7 @@ function htmlCreation(section, timedate, task, iscomplete) {
         completeBtn.innerText = "Completed";
         completeBtn.style.backgroundColor = "#8ac926";
     }
-    editDeleteCompleteBtn.append(completeBtn);
+    editDeleteCompleteBtn.append(deleteBtn, completeBtn);
     taskCard.append(editDeleteCompleteBtn);
     section.prepend(taskCard);
 }
@@ -57,10 +71,20 @@ function addTask(taskSection, taskObj) {
     htmlCreation(taskSection, taskObj.time, taskObj.task, taskObj.iscomplete);
 }
 
+function editTask(editTaskId) {
+    const currentTask = document.getElementById(editTaskId);
+    const taskText = currentTask.querySelector('.task-description');
+    inputField.value = taskText.innerText;
+    addBtn.style.display = "none";
+    updateBtn.style.display = "block";
+    updateBtn.setAttribute('data-updatebtn', editTaskId);
+}
+
 function deleteTask(deleteTaskId) {
     let addedTasks = JSON.parse(localStorage.getItem("todoTasks") || "[]");
     const deleteIndex = addedTasks.findIndex((ele) => ele.time === deleteTaskId);
-    if(addedTasks[deleteIndex].iscomplete) completedTasksCount.innerText = Number(completedTasksCount.innerText) - 1;
+    if (addedTasks[deleteIndex].iscomplete)
+        completedTasksCount.innerText = Number(completedTasksCount.innerText) - 1;
     else activeTasksCount.innerText = Number(activeTasksCount.innerText) - 1;
     addedTasks.splice(deleteIndex, 1);
     allTasksCount.innerText = addedTasks.length;
@@ -71,29 +95,34 @@ function deleteTask(deleteTaskId) {
 function completeTask(completeTaskId) {
     const currentTask = document.getElementById(completeTaskId);
     const completeBtn = currentTask.querySelector(".complete-btn");
+    const editBtn = currentTask.querySelector('.edit-btn');
     const taskDescription = currentTask.querySelector(".task-description");
     let addedTasks = JSON.parse(localStorage.getItem("todoTasks") || "[]");
-    const completeIndex = addedTasks.findIndex(ele => ele.time === completeTaskId);
-    if(addedTasks[completeIndex].iscomplete){
+    const completeIndex = addedTasks.findIndex(
+        (ele) => ele.time === completeTaskId
+    );
+    if (addedTasks[completeIndex].iscomplete) {
         taskDescription.style.textDecorationLine = "none";
         completeBtn.style.backgroundColor = "#023047";
         completeBtn.innerText = "Complete";
         addedTasks[completeIndex].iscomplete = false;
-        if(allOrActiveOrComplete === "Complete") taskSection.removeChild(currentTask);
-        if(allOrActiveOrComplete === "All" || allOrActiveOrComplete === "Complete"){
-            activeTasksCount.innerText = Number(activeTasksCount.innerText)+1;
-            completedTasksCount.innerText = Number(completedTasksCount.innerText)-1;
+        if (allOrActiveOrComplete === "All") editBtn.style.display = "block";
+        if (allOrActiveOrComplete === "Complete") taskSection.removeChild(currentTask);
+        if (allOrActiveOrComplete === "All" || allOrActiveOrComplete === "Complete") {
+            activeTasksCount.innerText = Number(activeTasksCount.innerText) + 1;
+            completedTasksCount.innerText = Number(completedTasksCount.innerText) - 1;
         }
-    }
-    else {
+    } else {
         taskDescription.style.textDecorationLine = "line-through";
         completeBtn.style.backgroundColor = "#8ac926";
         completeBtn.innerText = "Completed";
         addedTasks[completeIndex].iscomplete = true;
-        if (allOrActiveOrComplete === "Active") taskSection.removeChild(currentTask);
-        if(allOrActiveOrComplete === "All" || allOrActiveOrComplete === "Active"){
-            activeTasksCount.innerText = Number(activeTasksCount.innerText)-1;
-            completedTasksCount.innerText = Number(completedTasksCount.innerText)+1;
+        if(allOrActiveOrComplete === "All") editBtn.style.display = "none";
+        if (allOrActiveOrComplete === "Active")
+            taskSection.removeChild(currentTask);
+        if (allOrActiveOrComplete === "All" || allOrActiveOrComplete === "Active") {
+            activeTasksCount.innerText = Number(activeTasksCount.innerText) - 1;
+            completedTasksCount.innerText = Number(completedTasksCount.innerText) + 1;
         }
     }
     localStorage.setItem("todoTasks", JSON.stringify(addedTasks));
@@ -114,7 +143,7 @@ addBtn.addEventListener("click", () => {
         inputField.value = "";
         return;
     } else {
-        let obj = {
+        const obj = {
             time: timedate,
             task: taskText,
             iscomplete: false,
@@ -139,13 +168,18 @@ taskSection.addEventListener("click", (e) => {
         const completeTaskId = completeBtnId;
         completeTask(completeTaskId);
     }
+    if (e.target.className.includes("edit-btn")) {
+        const editBtnId = e.target.dataset.edittimedate;
+        const editTaskId = editBtnId;
+        editTask(editTaskId);
+    }
 });
 
 window.addEventListener("load", () => {
-    let addedTasks = JSON.parse(localStorage.getItem("todoTasks") || "[]");
+    const addedTasks = JSON.parse(localStorage.getItem("todoTasks") || "[]");
     let completeCnt = 0;
-    addedTasks.forEach(ele => {
-        if(ele.iscomplete) completeCnt++;
+    addedTasks.forEach((ele) => {
+        if (ele.iscomplete) completeCnt++;
         addTask(taskSection, ele);
     });
     allTasksCount.innerText = addedTasks.length;
@@ -165,9 +199,9 @@ completeShow.addEventListener("click", () => {
     allOrActiveOrComplete = "Complete";
     taskSection.innerHTML = "";
     let completeCnt = 0;
-    let addedTasks = JSON.parse(localStorage.getItem("todoTasks") || "[]");
-    addedTasks.forEach(ele => {
-        if (ele.iscomplete){
+    const addedTasks = JSON.parse(localStorage.getItem("todoTasks") || "[]");
+    addedTasks.forEach((ele) => {
+        if (ele.iscomplete) {
             htmlCreation(taskSection, ele.time, ele.task, ele.iscomplete);
             completeCnt++;
         }
@@ -180,22 +214,24 @@ activeShow.addEventListener("click", () => {
     allOrActiveOrComplete = "Active";
     taskSection.innerHTML = "";
     let activeCnt = 0;
-    let addedTasks = JSON.parse(localStorage.getItem("todoTasks") || "[]");
-    addedTasks.forEach(ele => {
-        if (!ele.iscomplete){
+    const addedTasks = JSON.parse(localStorage.getItem("todoTasks") || "[]");
+    addedTasks.forEach((ele) => {
+        if (!ele.iscomplete) {
             htmlCreation(taskSection, ele.time, ele.task, false);
             activeCnt++;
         }
     });
-    activeTasksCount.innerText = activeCnt; 
+    activeTasksCount.innerText = activeCnt;
 });
 
 allShow.addEventListener("click", () => {
     backgroundChange(allShow);
     allOrActiveOrComplete = "All";
     taskSection.innerHTML = "";
-    let addedTasks = JSON.parse(localStorage.getItem("todoTasks") || "[]");
-    addedTasks.forEach(ele => htmlCreation(taskSection, ele.time, ele.task, ele.iscomplete));
+    const addedTasks = JSON.parse(localStorage.getItem("todoTasks") || "[]");
+    addedTasks.forEach((ele) =>
+        htmlCreation(taskSection, ele.time, ele.task, ele.iscomplete)
+    );
     allTasksCount.innerText = addedTasks.length;
 });
 
@@ -203,8 +239,23 @@ clearCompleted.addEventListener("click", () => {
     backgroundChange(clearCompleted);
     taskSection.innerHTML = "";
     let addedTasks = JSON.parse(localStorage.getItem("todoTasks") || "[]");
-    addedTasks = addedTasks.filter(ele => ele.iscomplete === false);
+    addedTasks = addedTasks.filter((ele) => ele.iscomplete === false);
     allTasksCount.innerText = addedTasks.length;
     completedTasksCount.innerText = 0;
     localStorage.setItem("todoTasks", JSON.stringify(addedTasks));
 });
+
+updateBtn.addEventListener('click', (e) => {
+    const updateTaskId = e.target.dataset.updatebtn;
+    const updateTask = document.getElementById(updateTaskId);
+    const updateText = updateTask.querySelector('.task-description');
+    let addedTasks = JSON.parse(localStorage.getItem('todoTasks') || '[]');
+    addedTasks.forEach(ele => {
+        if (ele.time === updateTaskId) ele.task = inputField.value;
+    });
+    localStorage.setItem('todoTasks', JSON.stringify(addedTasks));
+    updateText.innerText = inputField.value;
+    updateBtn.style.display = "none";
+    addBtn.style.display = "block";
+    inputField.value = "";
+})
